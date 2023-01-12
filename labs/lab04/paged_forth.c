1c1d9bda54fe7d2a7ee59cd72f245345ed3e8026
brownjl5 <brownjl5@rose-hulman.edu>
2023-01-11 19:38:26 -0500

    Lab04 work

--- labs/lab04/paged_forth.c
+++ labs/lab04/paged_forth.c
@@ -18,12 +18,96 @@
 // the max number of pages we want in memort at once, ideally
 #define MAX_PAGES 3
 
+int  page_count = 0;
 
+int prio_list[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int page_list[3] = {};
+
+static void handler(int sig, siginfo_t *si, void *unused) 
+{
+    void* fault_address = si->si_addr;
+
+
+
+    printf("in handler with invalid address %p\n", fault_address);
+    int distance = (void*) fault_address - (void*) STACKHEAP_MEM_START;
+    if(distance < 0 || distance > getpagesize() * NUM_PAGES) {
+        printf("address not within expected page!\n");
+        exit(2);
+    }
+
+    int page_num = distance / getpagesize();
+
+    if(page_count >= 3) {
+        for(int i = 0; i < sizeof(page_list); i++) {
+            prio_list[page_list[i] - 1] -= 1;
+            if(prio_list[page_list[i] - 1] == 0){
+                int result = munmap((void*)  STACKHEAP_MEM_START + getpagesize() * page_list[i],
+                        getpagesize());
+                if(result == -1) {
+                    perror("map failed");
+                    exit(1);
+                }
+                printf("mapping page %d\n", page_num);
+                void* result2 = mmap((void*)  STACKHEAP_MEM_START + getpagesize() * page_num,
+                        getpagesize(),
+                        PROT_READ | PROT_WRITE | PROT_EXEC,
+                        MAP_FIXED | MAP_SHARED | MAP_ANONYMOUS,
+                        -1,
+                        0);
+                if(result2 == MAP_FAILED) {
+                    perror("map failed");
+                    exit(1);
+                }
+                page_list[i] = page_num;
+                prio_list[page_num - 1] = 3;
+            }
+        }
+    } else {
+        for(int i = 0; i < sizeof(page_list); i++) {
+            prio_list[page_list[i] - 1] -= 1;
+        }
+        printf("mapping page %d\n", page_num);
+        void* result = mmap((void*)  STACKHEAP_MEM_START + getpagesize() * page_num,
+                        getpagesize(),
+                        PROT_READ | PROT_WRITE | PROT_EXEC,
+                        MAP_FIXED | MAP_SHARED | MAP_ANONYMOUS,
+                        -1,
+                        0);
+        if(result == MAP_FAILED) {
+            perror("map failed");
+            exit(1);
+        }
+        prio_list[page_num - 1] = 3;
+        page_list[page_count] = page_num;
+        page_count += 1;
+    }
+}
 
 int main() {
 
     //TODO: Add a bunch of segmentation fault handler setup here for
-    //PART 1 (plus you'll also have to add the handler your self)
+    //PART 1 (plus you'll also have to add the handler your self) 
+    static char stack[SIGSTKSZ];
+    
+    stack_t ss = {
+                  .ss_size = SIGSTKSZ,
+                  .ss_sp = stack,
+    };
+    
+    sigaltstack(&ss, NULL);
+
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO | SA_ONSTACK;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handler;
+
+    if (sigaction(SIGSEGV, &sa, NULL) == -1) {
+        perror("error installing handler");
+        exit(3);
+    }
     
     struct forth_data forth;
     char output[200];
@@ -44,8 +128,7 @@ int main() {
     int stackheap_size = getpagesize() * NUM_PAGES;
 
     // TODO: Modify this in PART 1
-    void* stackheap = mmap(NULL, stackheap_size, PROT_READ | PROT_WRITE | PROT_EXEC,
-                   MAP_ANON | MAP_PRIVATE, -1, 0);
+    void* stackheap = (void*) STACKHEAP_MEM_START;
     
     
     initialize_forth_data(&forth,
